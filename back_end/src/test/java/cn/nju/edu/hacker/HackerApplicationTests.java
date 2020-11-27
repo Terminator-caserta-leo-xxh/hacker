55b2c3f23ae337d179102107d1d8b54b2acede83
xxh160 <55646006+xxh160@users.noreply.github.com>
2020-11-28 04:47:38 +0800

    first

---
+++ back_end/src/test/java/cn/nju/edu/hacker/HackerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nju.edu.hacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
