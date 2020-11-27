e7e1d79a9cdfeb0e86214a8c1de119d734d2657f
xxh160 <55646006+xxh160@users.noreply.github.com>
2020-11-28 04:58:27 +0800

    update .gitignore and git cache

---
+++ back_end/src/main/java/cn/nju/edu/hacker/HackerApplication.java
@@ -0,0 +1,13 @@
+package cn.nju.edu.hacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerApplication.class, args);
+	}
+
+}
