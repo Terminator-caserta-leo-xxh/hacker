0b2dd3b18927369c4a782197c51977929aeef9fc
Caserta-012 <2396813830@qq.com>
2020-11-28 18:52:30 +0800

    swiper UI

--- clients/src/App.vue
+++ clients/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "@/pages/Home";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
+    //HelloWorld
   }
 }
 </script>
---
+++ clients/src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!--菜品展示-->
+    <swiper :swiperList="swiperList"/>
+  </div>
+</template>
+
+<script>
+import Swiper from "@/components/Swiper";
+import {swiperList} from '@/assets/data/swiper';
+
+export default {
+name: "Home",
+  components: {Swiper},
+  data (){
+    return{
+        swiperList: [],
+    }
+  },
+  created() {
+    this.swiperList = swiperList
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
